7c6ab1f6f34de853c65121dd47e7602ef1cd046f
Rithy-Yano Hoy <rihoy@bess-f2r3s12.clusters.42paris.fr>
2024-12-30 15:50:37 +0100

    mariadb set

--- srcs/requirements/mariadb/tools/setup_db.sh
+++ srcs/requirements/mariadb/tools/setup_db.sh
@@ -1,8 +1,37 @@
 #!/bin/bash
 
+# Lecture secret
+DB_USERNAME=$(cat /run/secrets/db_username)
+DB_PASSWORD=$(cat /run/secrets/db_password)
+DB_ROOT_PASSWORD=$(cat /run/secrets/db_root_password)
+
+# Modification de l'interface reseaux pour permettre la connexion a partir de n'importe quel reseaux
+sed -i 's/bind-address\s*=\s*127\.0\.0\.1/bind-address = 0.0.0.0/' /etc/mysql/mariadb.conf.d/50-server.cnf
+
+# Demarrer le service
 service mariadb start
 
-mysql -u root -e "CREATE DATABASE openWorld;"
-mysql -u root -e "CREATE USER 'rihoy'@'host' IDENTIFIED BY 'password';"
-mysql -u root -e "GRANT ALL PRIVILEGES ON openWorld.* TO 'rihoy'@'host' IDENTIFIED BY 'password';"
-mysql -u root -e "FLUSH PRIVILEGES;"
+# Wait service has finish his initialisation
+sleep 2
+
+# Configuration
+if mariadb -e "USE $DB_DANAME;" 2> /dev/null; then
+	echo 'mariadb: DATA NAME Already exist'
+else
+	mysql -u root -e "CREATE DATABASE $DB_DANAME;"
+
+	mysql -u root -e "CREATE USER '$DB_USERNAME'@'%' IDENTIFIED BY '$DB_PASSWORD';"
+	mysql -u root -e "GRANT ALL PRIVILEGES ON $DB_DANAME.* TO '$DB_USERNAME'@'%' IDENTIFIED BY '$DB_PASSWORD' WITH GRANT OPTION;"
+	mysql -u root -e "GRANT ALL PRIVILEGES ON $DB_DANAME.* TO 'root'@'%' IDENTIFIED BY '$DB_ROOT_PASSWORD' WITH GRANT OPTION;"
+
+	mysql -u root -e "FLUSH PRIVILEGES;"
+fi
+
+# Arrete le service
+service mariadb stop
+
+# Wait service
+sleep 2
+
+# Lancier mariadb pour qu'il tourne en arriere plan
+exec mysqld_safe
