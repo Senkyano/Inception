7cde962ef945104684597506c694d891217a66cd
Senkyano <rithyyano.hoy@gmail.com>
2025-01-05 19:11:02 +0100

    test11

--- srcs/requirements/wordpress/tools/prompt.sh
+++ srcs/requirements/wordpress/tools/prompt.sh
@@ -8,12 +8,9 @@ mv wp-cli.phar /usr/local/bin/wp
 sed -i 's/listen\s*=\s*\/run\/php\/php7.4-fpm.sock/listen = 0.0.0.0:9000/' /etc/php/7.4/fpm/pool.d/www.conf
 sed -i 's/;pm.status_path = \/status/pm.status_path = \/php_fpm_status/' /etc/php/7.4/fpm/pool.d/www.conf
 
-
-cd /var/www/html/wordpress
-chmod -R 755 /var/www/html/wordpress
-
 wp core download --allow-root --path=/var/www/html/wordpress
 
+cd /var/www/html/wordpress
 wp config create --allow-root --dbname=${DB_DATABASE} --dbuser=${DB_USERNAME} --dbpass=${DB_PASSWORD} --dbhost=mariadb
 
 wp core install --allow-root --url=https://${DOMAIN_NAME} --title=${WP_TITLE} --admin_user=${WP_ADMIN} --admin_password=${WP_ADMIN_PASSWORD} --admin_email=${WP_ADMIN_EMAIL}
